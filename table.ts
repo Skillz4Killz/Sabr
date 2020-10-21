0ec31aa089b827d428843b74b4634a87456ad3e2
Skillz <gankstarsleague@gmail.com>
2020-10-21 12:30:58 -0400

    add new methods

--- table.ts
+++ table.ts
@@ -17,6 +17,8 @@ export class SabrTable<T> {
   }
 
   /** Get all documents from a table. */
+  async getAll(returnArray?: false): Promise<Map<string, T>>;
+  async getAll(returnArray?: true): Promise<T[]>;
   async getAll(returnArray = false) {
     const files = Deno.readDirSync(
       Deno.realPathSync(`${this.sabr.directoryPath}${this.name}`),
@@ -41,6 +43,68 @@ export class SabrTable<T> {
     }
 
     return returnArray ? [...data.values()] : data;
+  }
+
+  /** Get all documents from a table that match a filter */
+  async findMany(filter: Record<string, unknown> | ((value: T) => boolean), returnArray?: false): Promise<Map<string, T>>;
+  async findMany(filter: Record<string, unknown> | ((value: T) => boolean), returnArray?: true): Promise<T[]>;
+  async findMany(filter: Record<string, unknown> | ((value: T) => boolean), returnArray = false) {
+    const files = Deno.readDirSync(
+      Deno.realPathSync(`${this.sabr.directoryPath}${this.name}`),
+    );
+
+    const data = new Map<string, T>();
+
+    for (const file of files) {
+      if (!file.name || !file.isFile) continue;
+
+      try {
+        // Substring remove the file type `.json` from file.name
+        const name = file.name.substring(0, file.name.lastIndexOf("."));
+        const json = await this.get(name);
+        if (json) {
+          if (typeof filter === "function" ) {
+            if (filter(json)) data.set(name, json);
+          } else {
+            const invalid = Object.keys(filter).find(key => (json as Record<string, unknown>)[key] !== filter[key])
+            if (!invalid) data.set(name, json);
+          }
+        }
+      } catch (error) {
+        this.sabr.error(
+          `[Sabr Error: findMany]: Unable to read file ${this.sabr.directoryPath}${this.name}/${file.name}`,
+          error,
+        );
+      }
+    }
+
+    return returnArray ? [...data.values()] : data;
+  }
+
+  /** Gets the first document from a table that match a filter */
+  async findOne(filter: Record<string, unknown> | ((value: T) => boolean)) {
+    for await (const file of Deno.readDir(Deno.realPathSync(`${this.sabr.directoryPath}${this.name}`))) {
+      if (!file.name || !file.isFile) continue;
+
+      try {
+        // Substring remove the file type `.json` from file.name
+        const name = file.name.substring(0, file.name.lastIndexOf("."));
+        const json = await this.get(name);
+        if (json) {
+          if (typeof filter === "function" ) {
+            if (filter(json)) return json;
+          } else {
+            const invalid = Object.keys(filter).find(key => (json as Record<string, unknown>)[key] !== filter[key])
+            if (!invalid) return json;
+          }
+        }
+      } catch (error) {
+        this.sabr.error(
+          `[Sabr Error: findOne]: Unable to read file ${this.sabr.directoryPath}${this.name}/${file.name}`,
+          error,
+        );
+      }
+    }
   }
 
   /** Get a document from a table. */
@@ -83,8 +147,94 @@ export class SabrTable<T> {
     return this.create(id, existing ? { ...existing, ...data } : data);
   }
 
+  /** Gets the first document from a table that match a filter */
+  async updateOne(filter: Record<string, unknown> | ((value: T) => boolean), data: Record<string, unknown>) {
+    for await (const file of Deno.readDir(Deno.realPathSync(`${this.sabr.directoryPath}${this.name}`))) {
+      if (!file.name || !file.isFile) continue;
+
+      try {
+        // Substring remove the file type `.json` from file.name
+        const name = file.name.substring(0, file.name.lastIndexOf("."));
+        const json = await this.get(name);
+        if (json) {
+          if (typeof filter === "function" ) {
+            if (filter(json)) return this.update(name, data);
+          } else {
+            const invalid = Object.keys(filter).find(key => (json as Record<string, unknown>)[key] !== filter[key])
+            if (!invalid) return this.update(name, data);
+          }
+        }
+      } catch (error) {
+        this.sabr.error(
+          `[Sabr Error: updateOne]: Unable to read file ${this.sabr.directoryPath}${this.name}/${file.name}`,
+          error,
+        );
+      }
+    }
+  }
+
   /** Deletes a document from the table. */
   delete(id: string) {
     Deno.removeSync(`${this.sabr.directoryPath}${this.name}/${id}.json`);
+  }
+
+  /** Deletes one document in a table that match a filter */
+  async deleteOne(filter: Record<string, unknown> | ((value: T) => boolean)) {
+    const files = Deno.readDirSync(
+      Deno.realPathSync(`${this.sabr.directoryPath}${this.name}`),
+    );
+
+    for (const file of files) {
+      if (!file.name || !file.isFile) continue;
+
+      try {
+        // Substring remove the file type `.json` from file.name
+        const name = file.name.substring(0, file.name.lastIndexOf("."));
+        const json = await this.get(name);
+        if (json) {
+          if (typeof filter === "function" ) {
+            return this.delete(name)
+          } else {
+            const invalid = Object.keys(filter).find(key => (json as Record<string, unknown>)[key] !== filter[key])
+            if (!invalid) return this.delete(name);
+          }
+        }
+      } catch (error) {
+        this.sabr.error(
+          `[Sabr Error: deleteMany]: Unable to read file ${this.sabr.directoryPath}${this.name}/${file.name}`,
+          error,
+        );
+      }
+    }
+  }
+
+  /** Deletes all documents in a table that match a filter */
+  async deleteMany(filter: Record<string, unknown> | ((value: T) => boolean)) {
+    const files = Deno.readDirSync(
+      Deno.realPathSync(`${this.sabr.directoryPath}${this.name}`),
+    );
+
+    for (const file of files) {
+      if (!file.name || !file.isFile) continue;
+
+      try {
+        // Substring remove the file type `.json` from file.name
+        const name = file.name.substring(0, file.name.lastIndexOf("."));
+        const json = await this.get(name);
+        if (json) {
+          if (typeof filter === "function" ) {
+            this.delete(name)
+          } else {
+            const invalid = Object.keys(filter).find(key => (json as Record<string, unknown>)[key] !== filter[key])
+            if (!invalid) this.delete(name);
+          }
+        }
+      } catch (error) {
+        this.sabr.error(
+          `[Sabr Error: deleteMany]: Unable to read file ${this.sabr.directoryPath}${this.name}/${file.name}`,
+          error,
+        );
+      }
+    }
   }
 }
